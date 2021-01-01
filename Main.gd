183cd0ed1331b76fca39e744eb9ead1cf5f15ec2
Vilius Peteraitis <kovotojas@pop-os.localdomain>
2021-01-01 19:19:31 +0100

    works now

--- Main.gd
+++ Main.gd
@@ -9,7 +9,7 @@ var rng = RandomNumberGenerator.new()
 var ngram_freq = {}
 
 # [correct, total]
-var accuracy = [0,0]
+var accuracy = [0.0,0.0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,12 +28,11 @@ func register_choice(value):
 	update_history(value)
 	update_frequencies()
 	next_press = predict_next()
+	update_labels()
 	
 
 func update_history(keypress):
 	button_history.push_front(keypress)
-	$HBoxContainer/VBoxContainer2/ButtonHistory.text = "History of keypresses: " + \
-	String(button_history)
 
 func update_frequencies():
 	for n in range(2, min(Ngram_max,button_history.size())+1):
@@ -63,42 +62,20 @@ func predict_next():
 	# return was not reached. predict randomly:
 	return rng.randi_range(0,1)
 
-# Update ngram frequencies in Ngrams file
-func update_frequencies0():
-	if button_history.size()<5:
-		return
-	var last_5 = button_history.slice(0,4)
-	last_5.invert()
-	$"HBoxContainer/VBoxContainer2/Last 5".text = "Last 5 presses: " + \
-	String(last_5)
-	Ngrams.ngram_5[last_5] = 1 + Ngrams.ngram_5[last_5] 
-	$"HBoxContainer/VBoxContainer2/Frequency of last 5".text = "Frequency of last 5 ngrams: " +\
-	String(Ngrams.ngram_5[last_5])
-
-# takes last 4 pressess and predicts next keypress:
-func next_press_likelihood():
-	if button_history.size()<5:
-		next_press = rng.randi_range(0,1)
-		return
-	var last_4 = button_history.slice(0,3)
-	last_4.invert()
-	
-	var next_likelihood = [Ngrams.ngram_5[last_4 + [0]], \
-	Ngrams.ngram_5[last_4 + [1]]]
-	if Ngrams.ngram_5[last_4 + [0]] == Ngrams.ngram_5[last_4 + [1]]:
-		next_press = rng.randi_range(0,1)
-	if Ngrams.ngram_5[last_4 + [0]] > Ngrams.ngram_5[last_4 + [1]]:
-		next_press = 0
-	if Ngrams.ngram_5[last_4 + [0]] < Ngrams.ngram_5[last_4 + [1]]:
-		next_press = 1
-	$"HBoxContainer/VBoxContainer2/Next pred".text = String(next_likelihood) +\
-	"\nNext prediction: " + String(next_press)
 
 func update_acc(press):
 	accuracy[1]+=1
 	if next_press == press:
 		accuracy[0]+=1
-	print(accuracy)
+
+
+
+func update_labels():
+	$HBoxContainer/VBoxContainer2/Next_pred.text = "Next press prediction:" + \
+	String(next_press)
+	$HBoxContainer/VBoxContainer2/Acc.text = "Accuracy: " + \
+	String(accuracy[0]/accuracy[1]) + \
+	"\n Number of predictions: " + String(accuracy[1])
 
 func _on_Button_A_pressed():
 #	print("Button A pressed")
